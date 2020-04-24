01ad19b754c5e4f639be5d4f4b7cedac89702db9
JulianNavi <39164188+JulianNavi@users.noreply.github.com>
2020-04-24 18:59:40 +0300

    Initial Commit

---
+++ Tetris/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tetris
+//
+//  Created by Dzulis on 2020-04-24.
+//  Copyright © 2020 Dzulis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
